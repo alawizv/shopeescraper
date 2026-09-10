8354ca602895b3318d9a79049782c3366726edcb
AL <alawizaneva@gmail.com>
2026-09-10 13:58:53 +0700

    chore: tambah update.bat untuk git pull + reminder reload extension

---
+++ update.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "D:\Repo\Alawi\shopeescraper"
+echo Mengambil update dari GitHub...
+git pull
+echo.
+echo Selesai! Sekarang buka chrome://extensions dan klik refresh di extension Shopee Scraper.
+echo.
+pause
